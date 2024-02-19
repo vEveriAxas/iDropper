2334dfa03f557e034d7d60c3d8c9c15a704be329
vEveriAxas <v.everi@axas.ru>
2024-02-19 10:19:59 +0300

    add-navigationDrawer

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,16 +1,32 @@
 <template>
-    <div class="main-container">
-        <drawerPanelComp/>
+    <div class="main__container">
+        <drawerPanelComp :drawer-width="drawerWidth" />
+        <div class="main__content-block">
+            <filterPanelComp />
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import drawerPanelComp from '../components/navigationDrawer/drawerPanelComp';
+import filterPanelComp from '../components/filterPanel/filterPanelComp.vue';
+import { ref } from 'vue';
+
+const drawerWidth = ref(450);
+
 
 </script>
 
 <style scoped>
-.main-container {
+.main__container {
+    display: flex;
     height: 100%;
+    border: 1px solid black;
 }
+.main__content-block {
+    width: 100%;
+    height: 100%;
+    border: 1px solid black;
+}
+
 </style>
