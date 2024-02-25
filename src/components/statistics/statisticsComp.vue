82e0bfbeac3001952efdf96dc24d20afd893516c
AveryOn <lnkonion2@gmail.com>
2024-02-25 16:39:10 +0300

    add-base-statistics

---
+++ src/components/statistics/statisticsComp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="statistics">
+        Statistics        
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.statistics {
+    width: 100%;
+    height: 100%;
+    border: 1px solid black;
+    padding: 0.9rem 1rem 2rem 4rem;
+}
+</style>
--- src/router/mainRoutes.ts
+++ src/router/mainRoutes.ts
@@ -1,16 +1,27 @@
 import { RouteRecordRaw } from 'vue-router';
-import MainView from '@/views/MainView.vue';
+import MainView from '../views/MainView.vue';
 import hospitalsMainComp from '../components/hospitals/hospitalsMainComp.vue';
 import departmentsMainComp from '../components/departments/departmentsMainComp.vue';
 import hospitalRoomsMainComp from '../components/hospitalRooms/hospitalRoomsMainComp.vue';
 import hospitalBedsMainComp from '../components/hospitalBeds/hospitalBedsMainComp.vue';
+import statisticsComp from '../components/statistics/statisticsComp.vue';
 
 export default  [
     {
         path: '/main/',
         name:'main',
         component: MainView,
+        redirect: {name: 'statistics'},
         children: [
+            // СТАТИСТИКА
+            {
+                path: 'statistics',
+                name: 'statistics',
+                component: statisticsComp,
+                meta: {
+                    requireAuth: true,
+                }
+            },
             // БОЛЬНИЦЫ
             {
                 path: 'hospitals',
@@ -19,6 +30,7 @@ export default  [
                 meta: {
                     title: 'больницы',
                     listItemTitle: 'Больница',
+                    requireAuth: true,
                 }
             },
             // ОТДЕЛЕНИЯ
